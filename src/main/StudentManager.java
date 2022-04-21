0c12e677e6e73239b804ed01b042aaae88c9e524
Edyci123 <bicuedici@gmail.com>
2022-04-22 01:02:51 +0300

    ?

--- src/main/StudentManager.java
+++ src/main/StudentManager.java
@@ -8,7 +8,8 @@ public class StudentManager {
 	public static void main(String[] args) {
 		
 		// in order for this app to work you will have to have a database entitled firstSQL
-		Music.setMusic(); // initialize the array of songs to sent to new registered students
+		Mu
+		sic.setMusic(); // initialize the array of songs to sent to new registered students
 		new InsertCredentials(); // initialize the first page in which you should introduce the credentials of the database
 	}
 	
